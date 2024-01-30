31e65f296d5bc9e5efb0812756f6fedd04e0d1fd
Jayesh <jayeshbpatil8830@gmail.com>
2024-01-30 21:05:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+function toggleMenu() {
+  var nav = document.querySelector("nav");
+  nav.classList.toggle("show");
+}
+var navLinks = document.querySelectorAll("nav a");
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    toggleMenu(); // Hide the navigation when a link is clicked
+  });
+});
+VanillaTilt.init(document.querySelectorAll(".card"), {
+  max: 25,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.3,
+});
+
+
+
+
+$(function () {
+		
+  var filterList = {
+  
+      init: function () {
+      
+          // https://mixitup.kunkalabs.com/
+          $('#gallery').mixItUp({
+              selectors: {
+              target: '.gallery-item',
+              filter: '.filter'	
+          },
+          load: {
+            filter: '.print, .strategy, .logo, .web' // show all items on page load.
+          }     
+          });								
+      
+      }
+
+  };
+  
+  // Filter ALL the things
+  filterList.init();
+  
+});
+
+
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+// Navbar click event listeners
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetSectionId = this.getAttribute('href').substring(1); // Remove the '#' symbol
+    scrollToSection(targetSectionId);
+  });
+});
